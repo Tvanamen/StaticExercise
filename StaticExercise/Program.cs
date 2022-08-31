b0bd40b9aba160d6dc6975e15e3a3b5fdcf201fa
Tanner Van Amen <Tvanamen@yahoo.com>
2022-08-30 19:25:53 -0600

    inital commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a tempeture in Fahrenheit");
+           var fahrenheit = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Your temp before conversion was {fahrenheit}");
+           //TempConverter.FahrenheitToCelsius(fahrenheit);
+           Console.WriteLine($"Your temp after conversion is {TempConverter.FahrenheitToCelsius(fahrenheit)}");
+            Console.WriteLine("Please enter a tempeture is Celsius");
+            var celsius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Your temp before conversion was {celsius}");
+            //TempConverter.CelsiusToFahrenheit(celsius);
+            Console.WriteLine($"Your temp after conversion is {TempConverter.CelsiusToFahrenheit(celsius)}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double Fahrenheit)
+        {
+            var result = (Fahrenheit - 32) / 1.8;
+
+            return result;
+        }
+        public static double CelsiusToFahrenheit(double Celsius)
+        {
+            return (Celsius * 9) / 5 + 32;
+        }
+    }
+}
